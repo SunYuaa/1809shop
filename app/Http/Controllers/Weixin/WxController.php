3fb320c106a1dec7bf18597723a6a11a7341c03a
SunYuaa <2861164631@qq.com>
2019-04-11 12:31:44 +0000

    p

--- app/Http/Controllers/Weixin/WxController.php
+++ app/Http/Controllers/Weixin/WxController.php
@@ -44,7 +44,7 @@ class WxController extends Controller
             ];
             $local_user = WxUserModel::where($where)->get();
             var_dump($local_user);
-            if ($local_user) {   //之前关注过
+            if (!empty($local_user)) {   //之前关注过
                 echo '之前关注过';
                 echo '<xml><ToUserName><![CDATA[' . $openid . ']]></ToUserName><FromUserName><![CDATA[' . $wx_id. ']]></FromUserName><CreateTime>' . time() . '</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA[' . '欢迎回来' . $local_user['nickname'] . ']]></Content></xml>';
             } else {             //首次关注
